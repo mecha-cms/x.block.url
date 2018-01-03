3433750fd2cff951147843d79a7339d4dcb20123
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-01-03 09:14:17 +0700

    Small Bug Fix

--- block.url/index.php
+++ block.url/index.php
@@ -3,13 +3,13 @@
 function fn_block_url($content) {
     $state = Extend::state('block', 'union');
     $o = $state[1][0][0]; // `[[`
-    $url = __url__();
+    $url = new URL;
     if (strpos($content, $o . 'url.') !== false) {
         foreach (array_keys($url) as $v) {
-            $content = Block::replace('url.' . $v, $url[$v], $content);
+            $content = Block::replace('url.' . $v, $url->{$v}, $content);
         }
     }
-    return Block::replace('url', $url['url'], $content);
+    return Block::replace('url', $url . "", $content);
 }
 
 Block::set('url', 'fn_block_url', 10);
