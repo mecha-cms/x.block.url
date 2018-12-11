2de38b7ea93603041f46291b3b036ca6956f37f1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-11 19:55:52 +0700

    Update

--- block.url/index.php
+++ block.url/index.php
@@ -1,15 +1,9 @@
-<?php
+<?php namespace fn\block;
 
-function fn_block_url($content) {
-    $state = Extend::state('block', 'union');
-    $o = $state[1][0][0]; // `[[`
-    $url = new URL;
-    if (strpos($content, $o . 'url.') !== false) {
-        foreach (array_keys($url) as $v) {
-            $content = Block::replace('url.' . $v, $url->{$v}, $content);
-        }
-    }
-    return Block::replace('url', $url . "", $content);
+function url() {} // Reserved function
+
+foreach ($GLOBALS['URL'] as $k => $v) {
+    \Block::set('url.' . $k, $v, 10);
 }
 
-Block::set('url', 'fn_block_url', 10);
+\Block::set('url', $url . "", 10);
