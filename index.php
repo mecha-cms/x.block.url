871eb70a1a3d20185ebd948378a93290f3e0cb20
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-15 10:45:23 +0700

    Stable

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php namespace x;
 
-function block__url(string $content, array $data = []) {
-    if (0 === \strpos($data[0], 'url.')) {
-        return (string) ($GLOBALS['url']->{\substr($data[0], 4)} ?? "");
+function block__url(string $content, array $lot = []) {
+    if (0 === \strpos($lot[0], 'url.')) {
+        return (string) ($GLOBALS['url']->{\substr($lot[0], 4)} ?? "");
     }
-    if ('url' === $data[0]) {
+    if ('url' === $lot[0]) {
         return (string) ($GLOBALS['url'] ?? "");
     }
     return "";
